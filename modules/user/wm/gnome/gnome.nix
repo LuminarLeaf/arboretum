df91be4a58683b573f05d3082d04809ef04ee2a6
Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>
2024-10-30 23:15:28 +0530

    gnome cleanup #nomore catppuccin gtk

--- modules/user/wm/gnome/gnome.nix
+++ modules/user/wm/gnome/gnome.nix
@@ -1,37 +1,60 @@
-{pkgs, ...}: {
-  programs = {
-    gnome-shell = {
-      enable = true;
-      theme = {
-        name = "Marble-purple-dark";
-        package = pkgs.marble-shell-theme.override {additionalInstallationTweaks = ["--filled"];};
-      };
-      extensions = [
-        {package = pkgs.gnomeExtensions.clipboard-indicator;}
-        {package = pkgs.gnomeExtensions.dash-to-dock;}
-        {package = pkgs.gnomeExtensions.appindicator;}
-        {package = pkgs.gnomeExtensions.blur-my-shell;}
-        {package = pkgs.gnomeExtensions.window-is-ready-remover;}
-        {package = pkgs.gnomeExtensions.pip-on-top;}
-      ];
-    };
+{pkgs, ...}: let
+  gtk-theme = pkgs.tokyonight-gtk-theme.override {
+    colorVariants = ["dark"];
+    sizeVariants = ["standard"];
+    themeVariants = ["purple"];
   };
+  gtk-theme-name = "Tokyonight-Purple-Dark";
+  gtk4dir = "${gtk-theme}/share/themes/${gtk-theme-name}/gtk-4.0";
+in {
+  programs.gnome-shell = {
+    enable = true;
+    theme = {
+      name = "Marble-purple-dark";
+      package = pkgs.marble-shell-theme.override {
+        additionalInstallationTweaks = ["--filled"];
+        colors = ["purple"];
+      };
+    };
+    extensions = [
+      {package = pkgs.gnomeExtensions.clipboard-indicator;}
+      {package = pkgs.gnomeExtensions.dash-to-dock;}
+      {package = pkgs.gnomeExtensions.appindicator;}
+      {package = pkgs.gnomeExtensions.blur-my-shell;}
+      {package = pkgs.gnomeExtensions.window-is-ready-remover;}
+      {package = pkgs.gnomeExtensions.pip-on-top;}
+    ];
+  };
+
+  gtk = {
+    cursorTheme.name = "Qogir-dark";
+    iconTheme.name = "Reversal-purple-dark";
+    theme.name = gtk-theme-name;
+  };
+
   home.packages = with pkgs; [
     gnome-tweaks
-    qogir-icon-theme
-    # rose-pine-gtk-theme
-    # omni-gtk-theme
-    # kanagawa-gtk-theme
-    # colloid-gtk-theme
-    # fluent-gtk-theme
-    # nightfox-gtk-theme
-    # whitesur-gtk-theme
-    # tokyonight-gtk-theme
-    # graphite-gtk-theme
+    # TODO: somehow patch reversal's foder icons into whitesur along with qogir's cursors in a curstom derivation
+    (qogir-icon-theme.override {
+      colorVariants = ["dark"];
+      themeVariants = ["default"];
+    })
+    (reversal-icon-theme.override {colorVariants = ["-purple"];})
+    gtk-theme
 
     # TODO: find which gsettings to have by default
     dconf-editor
   ];
+
+  home.sessionVariables = {
+    GTK_THEME = gtk-theme-name;
+  };
+
+  xdg.configFile = {
+    "gtk-4.0/assets".source = "${gtk4dir}/assets";
+    "gtk-4.0/gtk.css".source = "${gtk4dir}/gtk.css";
+    "gtk-4.0/gtk-dark.css".source = "${gtk4dir}/gtk-dark.css";
+  };
 
   xdg.mimeApps.defaultApplications = {
     "text/plain" = "org.gnome.TextEditor.desktop";
