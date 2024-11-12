4ff2a2a5d0c42721cebcd811b553a87a3894e095
Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>
2024-11-13 02:20:41 +0530

    bat man fix

--- modules/user/shell/tools/bat.nix
+++ modules/user/shell/tools/bat.nix
@@ -11,13 +11,15 @@
   programs.zsh = {
     shellAliases.cat = "bat --style header,snip,changes,numbers";
     sessionVariables = {
-      MANPAGER = "sh -c 'col -bx | bat -l man -p'";
+      # MANPAGER = "sh -c 'col -bx | bat -l man -p'";
+      MANPAGER = ''sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman' '';
       PAGER = "bat";
     };
     initExtra = ''
       bh() {
         "$@" --help 2>&1 | bat -pl help
       }
+      compdef '_command_names -e' bh
     '';
   };
 }
