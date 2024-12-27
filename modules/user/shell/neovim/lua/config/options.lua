1b86c751161fbc4b65a04494f4ced197d5f360c8
Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>
2024-12-28 01:21:56 +0530

    not necessary

--- modules/user/shell/neovim/lua/config/options.lua
+++ modules/user/shell/neovim/lua/config/options.lua
@@ -2,6 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.o.ignorecase = true
-vim.o.relativenumber = true
 -- vim.o.scrolloff = 999
