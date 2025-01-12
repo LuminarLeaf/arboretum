bc663cbd12ecff1c0c7b8afcac1fbb8d25ad239b
Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>
2025-01-12 19:47:12 +0530

    refactor a lil'
    Signed-off-by: Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -3,19 +3,19 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    # nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
-
-    alejandra = {
-      url = "github:kamadorueda/alejandra/3.0.0";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
 
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    alejandra = {
+      url = "github:kamadorueda/alejandra/3.0.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     catppuccin.url = "github:catppuccin/nix";
+
     spicetify-nix = {
       url = "github:Gerg-L/spicetify-nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -27,22 +27,23 @@
     };
   };
 
-  outputs = {self, ...} @ inputs: let
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: let
     userSettings = {
       username = "leaf";
       name = "Luminar Leaf";
       git_email = "80571430+LuminarLeaf@users.noreply.github.com";
     };
     system = "x86_64-linux";
-    pkgs = import inputs.nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    };
+    pkgs = nixpkgs.legacyPackages.${system};
   in {
     formatter.${system} = inputs.alejandra.defaultPackage.${system};
 
     nixosConfigurations = {
-      maple = inputs.nixpkgs.lib.nixosSystem {
+      maple = nixpkgs.lib.nixosSystem {
         inherit system;
         specialArgs = {
           inherit userSettings;
