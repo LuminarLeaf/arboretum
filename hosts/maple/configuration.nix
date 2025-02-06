3893f83c95f4ff4a9c2e6f0320148ce2b50dfe5e
Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>
2025-02-07 03:40:44 +0530

    add nix-community binary cache
    Signed-off-by: Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>

--- hosts/maple/configuration.nix
+++ hosts/maple/configuration.nix
@@ -40,6 +40,12 @@
     settings = {
       experimental-features = ["nix-command" "flakes"];
       trusted-users = ["@wheel"];
+      substituters = [
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
     nixPath = ["nixpkgs=${inputs.nixpkgs}"];
   };
