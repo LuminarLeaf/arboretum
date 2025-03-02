84e80406f838a418efbae3e89a5f144834ced474
Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>
2025-03-03 00:45:20 +0530

    treesitter highlighting
    Signed-off-by: Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>

--- modules/user/shell/zsh.nix
+++ modules/user/shell/zsh.nix
@@ -22,7 +22,6 @@ in {
 
     zsh = {
       enable = true;
-      # zprof.enable = true;
       shellAliases = aliases;
       defaultKeymap = "viins";
       antidote = {
@@ -54,87 +53,91 @@ in {
         share = true;
         size = 50000;
       };
-      initExtraFirst = ''
-        # needs to be loaded before zsh-syntax-highlighting
-        autoload -U select-word-style
-        select-word-style bash
-      '';
-      initExtra = ''
-        setopt APPEND_HISTORY
-        setopt HIST_FIND_NO_DUPS
+      initExtraFirst =
+        #bash
+        ''
+          # needs to be loaded before zsh-syntax-highlighting
+          autoload -U select-word-style
+          select-word-style bash
+        '';
+      initExtra =
+        #bash
+        ''
+          setopt APPEND_HISTORY
+          setopt HIST_FIND_NO_DUPS
 
-        zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
-        zstyle ':completion:*' list-colors "$\{(s.:.)LS_COLORS}"
-        zstyle ':completion:*' menu no
-        zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color always $realpath'
-        zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'eza -1 --color always $realpath'
+          zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+          zstyle ':completion:*' list-colors "$\{(s.:.)LS_COLORS}"
+          zstyle ':completion:*' menu no
+          zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color always $realpath'
+          zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'eza -1 --color always $realpath'
 
-        bindkey '^a' beginning-of-line
-        bindkey '^e' end-of-line
-        bindkey '^?' backward-delete-char
-        bindkey '^f' forward-char
-        bindkey '^[b' vi-backward-word
-        bindkey '^[f' vi-forward-word
-        bindkey '^[n' down-line-or-history
-        bindkey '^[p' up-line-or-history
-        bindkey '^[^?' backward-kill-word
+          bindkey '^a' beginning-of-line
+          bindkey '^e' end-of-line
+          bindkey '^?' backward-delete-char
+          bindkey '^f' forward-char
+          bindkey '^[b' vi-backward-word
+          bindkey '^[f' vi-forward-word
+          bindkey '^[n' down-line-or-history
+          bindkey '^[p' up-line-or-history
+          bindkey '^[^?' backward-kill-word
 
-        # HACK: This breaks fzf-tab in tmux for some reason, remove if you want to use tmux AND fzf-tab
-        zstyle ':fzf-tab:*' use-fzf-default-opts yes
+          # HACK: This breaks fzf-tab in tmux for some reason, remove if you want to use tmux AND fzf-tab
+          zstyle ':fzf-tab:*' use-fzf-default-opts yes
 
-        check() {
-          if command -v $1 &> /dev/null; then return 0; else return 1; fi
-        }
+          check() {
+            if command -v $1 &> /dev/null; then return 0; else return 1; fi
+          }
 
-        ex() {
-          if [ $# -eq 0 ]; then
-            echo "error: no <file> provided for extraction"
-            echo "usage: ex <file>"
-          fi
-          while [ $# -ne 0 ]; do
-            if [ ! -f $1 ]; then
-              echo "'$1' is not a valid file"
+          ex() {
+            if [ $# -eq 0 ]; then
+              echo "error: no <file> provided for extraction"
+              echo "usage: ex <file>"
             fi
-            case $1 in
-              *.tar.bz2) tar xjf $1 ;;
-              *.tar.gz) tar xzf $1 ;;
-              *.tbz2) tar xjf $1 ;;
-              *.tgz) tar xzf $1 ;;
-              *.tar) tar xf $1 ;;
-              *.bz2) bunzip2 $1 ;;
-              *.rar) unrar x $1 ;;
-              *.gz) gunzip $1 ;;
-              *.zip) unzip $1 ;;
-              *.Z) uncompress $1 ;;
-              *.7z) 7z x $1 ;;
-              *) echo "'$1' cannot be extracted via ex()" ;;
-            esac
-            shift
-          done
-        }
+            while [ $# -ne 0 ]; do
+              if [ ! -f $1 ]; then
+                echo "'$1' is not a valid file"
+              fi
+              case $1 in
+                *.tar.bz2) tar xjf $1 ;;
+                *.tar.gz) tar xzf $1 ;;
+                *.tbz2) tar xjf $1 ;;
+                *.tgz) tar xzf $1 ;;
+                *.tar) tar xf $1 ;;
+                *.bz2) bunzip2 $1 ;;
+                *.rar) unrar x $1 ;;
+                *.gz) gunzip $1 ;;
+                *.zip) unzip $1 ;;
+                *.Z) uncompress $1 ;;
+                *.7z) 7z x $1 ;;
+                *) echo "'$1' cannot be extracted via ex()" ;;
+              esac
+              shift
+            done
+          }
 
-        mkcd() {
-          if [ $# -eq 0 ]; then
-            echo "error: no <dir> provided for creation"
-            echo "usage: mkcd <dir>"
-          fi
-          mkdir -p $1 && cd $1
-        }
+          mkcd() {
+            if [ $# -eq 0 ]; then
+              echo "error: no <dir> provided for creation"
+              echo "usage: mkcd <dir>"
+            fi
+            mkdir -p $1 && cd $1
+          }
 
-        e() {
-          if [[ -n "$VISUAL" ]]; then
-            "$VISUAL" "$@"
-          elif [[ -n "$EDITOR" ]]; then
-            "$EDITOR" "$@"
-          else
-            echo "No editor set"
-          fi
-        }
+          e() {
+            if [[ -n "$VISUAL" ]]; then
+              "$VISUAL" "$@"
+            elif [[ -n "$EDITOR" ]]; then
+              "$EDITOR" "$@"
+            else
+              echo "No editor set"
+            fi
+          }
 
-        ddiso() {
-          dd if=$1 of=$2 status=progress oflag=sync
-        }
-      '';
+          ddiso() {
+            dd if=$1 of=$2 status=progress oflag=sync
+          }
+        '';
     };
   };
 }
