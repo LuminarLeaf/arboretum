939f93febacb4fe2f7e8dc8916181e9aabad1bb9
Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>
2025-04-07 10:23:42 +0530

    sub ended, zsh didnt sync across sessions anymore
    Signed-off-by: Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>

--- modules/user/shell/zsh.nix
+++ modules/user/shell/zsh.nix
@@ -50,15 +50,12 @@ in {
         ];
       };
       history = {
-        append = true;
-        findNoDups = true;
         ignoreAllDups = true;
         ignoreDups = true;
         ignorePatterns = ["rm *" "pkill *"];
         ignoreSpace = true;
         path = "$HOME/.zsh_history";
         save = 10000;
-        saveNoDups = true;
         share = true;
         size = 50000;
       };
