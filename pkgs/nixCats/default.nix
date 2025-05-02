35da8267ae1f691548a45499ea60761a6ce34fcf
Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>
2025-05-02 11:43:21 +0530

    add nixCats package
    Signed-off-by: Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>

---
+++ pkgs/nixCats/default.nix
@@ -0,0 +1,167 @@
+{
+  nixCats,
+  nixpkgs,
+  system,
+  ...
+}: let
+  inherit (nixCats) utils;
+  luaPath = "${./.}";
+  extra_pkg_config = {};
+
+  dependencyOverlays = [];
+  categoryDefinitions = {
+    pkgs,
+    settings,
+    categories,
+    extra,
+    name,
+    mkPlugin,
+    ...
+  } @ packageDef: {
+    lspsAndRuntimeDeps = with pkgs; {
+      general = [
+        curl
+        fd
+        ripgrep
+        stdenv.cc.cc
+        universal-ctags
+
+        (pkgs.writeShellScriptBin "lazygit" ''
+          exec ${pkgs.lazygit}/bin/lazygit --use-config-file ${pkgs.writeText "lazygit_config.yml" ""} "$@"
+        '')
+
+        bacon
+        bash-language-server
+        biome
+        black
+        clang-tools
+        delve
+        docker-compose-language-service
+        dockerfile-language-server-nodejs
+        gofumpt
+        gopls
+        lua-language-server
+        markdownlint-cli2
+        marksman
+        nixd
+        pyright
+        ruff
+        rust-analyzer
+        shellcheck
+        shfmt
+        sqls
+        stylua
+        taplo
+        typescript-language-server
+        vale
+        vscode-js-debug
+        vscode-langservers-extracted
+        vtsls
+        yaml-language-server
+      ];
+    };
+
+    startupPlugins = {};
+
+    optionalPlugins = with pkgs.vimPlugins; {
+      general = [
+        lazy-nvim
+        LazyVim
+
+        blink-cmp
+        bufferline-nvim
+        clangd_extensions-nvim
+        conform-nvim
+        CopilotChat-nvim
+        flash-nvim
+        friendly-snippets
+        gitsigns-nvim
+        grug-far-nvim
+        lazydev-nvim
+        lualine-nvim
+        markdown-preview-nvim
+        neo-tree-nvim
+        noice-nvim
+        nui-nvim
+        nvim-dap
+        nvim-dap-python
+        nvim-dap-ui
+        nvim-dap-virtual-text
+        nvim-lint
+        nvim-lspconfig
+        nvim-nio
+        nvim-treesitter-context
+        nvim-treesitter-textobjects
+        nvim-treesitter.withAllGrammars
+        nvim-ts-autotag
+        nvim-web-devicons
+        persistence-nvim
+        plenary-nvim
+        render-markdown-nvim
+        SchemaStore-nvim
+        snacks-nvim
+        telescope-fzf-native-nvim
+        telescope-nvim
+        todo-comments-nvim
+        tokyonight-nvim
+        trouble-nvim
+        ts-comments-nvim
+        vim-illuminate
+        vim-startuptime
+        which-key-nvim
+        yanky-nvim
+
+        {
+          plugin = catppuccin-nvim;
+          name = "catppuccin";
+        }
+        {
+          plugin = harpoon2;
+          name = "harpoon";
+        }
+        {
+          plugin = mini-ai;
+          name = "mini.ai";
+        }
+        {
+          plugin = mini-hipatterns;
+          name = "mini.hipatterns";
+        }
+        {
+          plugin = mini-icons;
+          name = "mini.icons";
+        }
+        {
+          plugin = mini-pairs;
+          name = "mini.pairs";
+        }
+      ];
+    };
+  };
+
+  packageDefinitions.nvim = {
+    pkgs,
+    name,
+    mkPlugin,
+    ...
+  }: {
+    settings = {
+      suffix-path = true;
+      suffix-LD = true;
+      wrapRc = true;
+      # aliases = ["vim"];
+      hosts.python3.enable = true;
+      hosts.node.enable = true;
+    };
+    categories = {
+      general = true;
+    };
+    extra = {};
+  };
+in
+  utils.baseBuilder luaPath {
+    inherit nixpkgs system dependencyOverlays extra_pkg_config;
+  }
+  categoryDefinitions
+  packageDefinitions
+  "nvim"
---
+++ pkgs/nixCats/init.lua
@@ -0,0 +1,87 @@
+-- NOTE: this just gives nixCats global command a default value
+-- so that it doesnt throw an error if you didnt install via nix.
+-- usage of both this setup and the nixCats command is optional,
+-- but it is very useful for passing info from nix to lua so you will likely use it at least once.
+require('nixCatsUtils').setup {
+  non_nix_value = true,
+}
+
+-- NOTE: You might want to move the lazy-lock.json file
+local function getlockfilepath()
+  if require('nixCatsUtils').isNixCats and type(nixCats.settings.unwrappedCfgPath) == 'string' then
+    return nixCats.settings.unwrappedCfgPath .. '/lazy-lock.json'
+  else
+    return vim.fn.stdpath 'config' .. '/lazy-lock.json'
+  end
+end
+local lazyOptions = {
+  lockfile = getlockfilepath(),
+}
+
+-- NOTE: this the lazy wrapper. Use it like require('lazy').setup() but with an extra
+-- argument, the path to lazy.nvim as downloaded by nix, or nil, before the normal arguments.
+require('nixCatsUtils.lazyCat').setup(nixCats.pawsible { 'allPlugins', 'start', 'lazy.nvim' }, {
+  { 'LazyVim/LazyVim', import = 'lazyvim.plugins' },
+
+  { 'williamboman/mason-lspconfig.nvim', enabled = require('nixCatsUtils').lazyAdd(true, false) },
+  { 'williamboman/mason.nvim', enabled = require('nixCatsUtils').lazyAdd(true, false) },
+  { 'jay-babu/mason-nvim-dap.nvim', enabled = require('nixCatsUtils').lazyAdd(true, false) },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = require('nixCatsUtils').lazyAdd ':TSUpdate',
+    opts_extend = require('nixCatsUtils').lazyAdd(nil, false),
+    opts = {
+      ensure_installed = require('nixCatsUtils').lazyAdd(
+        { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+        false
+      ),
+      auto_install = require('nixCatsUtils').lazyAdd(true, false),
+      ignore_install = require('nixCatsUtils').lazyAdd(nil, 'all'),
+    },
+  },
+  {
+    'folke/lazydev.nvim',
+    opts = { library = { { path = (nixCats.nixCatsPath or '') .. '/lua', words = { 'nixCats' } } } },
+  },
+
+  { import = 'lazyvim.plugins.extras.ai.copilot-chat' },
+  { import = 'lazyvim.plugins.extras.coding.blink' },
+  { import = 'lazyvim.plugins.extras.coding.yanky' },
+  { import = 'lazyvim.plugins.extras.dap.core' },
+  { import = 'lazyvim.plugins.extras.editor.harpoon2' },
+  { import = 'lazyvim.plugins.extras.editor.neo-tree' },
+  { import = 'lazyvim.plugins.extras.editor.snacks_picker' },
+  { import = 'lazyvim.plugins.extras.formatting.biome' },
+  { import = 'lazyvim.plugins.extras.formatting.black' },
+  { import = 'lazyvim.plugins.extras.lang.clangd' },
+  { import = 'lazyvim.plugins.extras.lang.docker' },
+  { import = 'lazyvim.plugins.extras.lang.git' },
+  { import = 'lazyvim.plugins.extras.lang.go' },
+  { import = 'lazyvim.plugins.extras.lang.json' },
+  { import = 'lazyvim.plugins.extras.lang.markdown' },
+  { import = 'lazyvim.plugins.extras.lang.python' },
+  { import = 'lazyvim.plugins.extras.lang.toml' },
+  { import = 'lazyvim.plugins.extras.lang.typescript' },
+  { import = 'lazyvim.plugins.extras.lang.yaml' },
+  { import = 'lazyvim.plugins.extras.ui.treesitter-context' },
+  { import = 'lazyvim.plugins.extras.util.dot' },
+  { import = 'lazyvim.plugins.extras.util.mini-hipatterns' },
+  -- import/override with your plugins
+  { import = 'plugins' },
+}, lazyOptions)
+
+if vim.g.neovide then
+  -- Display
+  -- vim.o.guifont = "Maple Mono NF:h12:#h-none"
+  vim.g.neovide_opacity = 1
+  vim.g.neovide_floating_corner_radius = 0.25
+  vim.g.neovide_hide_mouse_when_typing = true
+
+  -- Functionality
+  vim.g.neovide_refresh_rate = 144
+  vim.g.neovide_refresh_rate_idle = 10
+
+  -- Cursor
+  vim.g.neovide_cursor_vfx_mode = 'pixiedust'
+  vim.g.neovide_cursor_vfx_particle_density = 10
+end
