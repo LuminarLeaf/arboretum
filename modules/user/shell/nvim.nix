18d90112a1a64f1965d2c3b374336118b65dd7fb
Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>
2025-05-02 11:43:55 +0530

    nuke raw nvim config and add nixCats package
    Signed-off-by: Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>

--- modules/user/shell/nvim.nix
+++ modules/user/shell/nvim.nix
@@ -1,55 +1,11 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    # base packages
-    neovim
-    neovim-remote
-
-    nixd
-
-    vscode-langservers-extracted # html css json eslint
-
-    marksman # md
-    markdownlint-cli2
-    vale # md lint
-
-    lua-language-server
-    stylua
-
-    bash-language-server
-    shfmt
-
-    yaml-language-server
-    taplo
-
-    dockerfile-language-server-nodejs
-    docker-compose-language-service
-
-    typescript-language-server
-    biome # ts js
-    vtsls
-
-    rust-analyzer
-    bacon # rust lint
-
-    sqls # sql mysql
-
-    pyright # python lsp
-    ruff # python formatter + lsp
-
-    gopls # go lsp
-    gofumpt
-
-    clang-tools # clangd
+{
+  pkgs,
+  inputs,
+  ...
+}: {
+  home.packages = [
+    inputs.self.packages.${pkgs.system}.nixCats
   ];
-
-  home.file.".config/nvim" = {
-    source = ./neovim;
-    recursive = true;
-  };
-  # systemd.user.tmpfiles.rules = [
-  #   "L+ /home/leaf/.config/nvim/lazy-lock.json - - - - /home/leaf/arboretum/modules/user/shell/neovim/lazy-lock.json"
-  #   "L+ /home/leaf/.config/nvim/lazyvim.json - - - - /home/leaf/arboretum/modules/user/shell/neovim/lazyvim.json"
-  # ];
 
   programs.neovide = {
     enable = true;
