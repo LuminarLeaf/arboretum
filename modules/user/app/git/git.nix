df36ec716ae08ff36cda21366ce418952e3bbfc3
Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>
2025-05-02 14:36:14 +0530

    git url aliases
    Signed-off-by: Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>

--- modules/user/app/git/git.nix
+++ modules/user/app/git/git.nix
@@ -10,6 +10,10 @@
         init.defaultBranch = "main";
         gpg.format = "ssh";
         user.signingkey = "~/.ssh/id_ed25519.pub";
+        url = {
+          "git@github.com:" = {insteadOf = "gh:";};
+          "git@github.com:LuminarLeaf/" = {insteadOf = "ll:";};
+        };
       };
     };
     gh = {
