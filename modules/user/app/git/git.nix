2d5b753deedf4e6d852bb9dbc3a84017ece41d51
Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>
2025-05-08 12:59:20 +0530

    enable git auto sign
    Signed-off-by: Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>

--- modules/user/app/git/git.nix
+++ modules/user/app/git/git.nix
@@ -8,6 +8,7 @@
       userEmail = userSettings.git_email;
       extraConfig = {
         init.defaultBranch = "main";
+        commit.gpgsign = true;
         gpg.format = "ssh";
         user.signingkey = "~/.ssh/id_ed25519.pub";
         url = {
