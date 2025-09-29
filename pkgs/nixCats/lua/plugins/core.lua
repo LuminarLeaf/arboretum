961f731929f29332b9b6bafe5fd933c74f82cd2b
Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>
2025-09-30 02:36:04 +0530

    flake lock update
    Signed-off-by: Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>

--- pkgs/nixCats/lua/plugins/core.lua
+++ pkgs/nixCats/lua/plugins/core.lua
@@ -9,14 +9,6 @@ return {
     'catppuccin/nvim',
     opts = { term_colors = true },
   },
-  -- TODO: Remove this once https://github.com/LazyVim/LazyVim/pull/6354 is merged or I move to nvf
-  {
-    'akinsho/bufferline.nvim',
-    init = function()
-      local bufline = require 'catppuccin.groups.integrations.bufferline'
-      bufline.get = bufline.get_theme
-    end,
-  },
   {
     'folke/which-key.nvim',
     opts = {
