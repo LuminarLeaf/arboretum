74b66939fc385581637a4194ae5fbf8bab276376
Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>
2025-12-06 11:32:46 +0530

    Revert "s3 sleep always"
    This reverts commit df15d7aba253f94b41ae93f86a5afef0fc845073.

--- hosts/maple/configuration.nix
+++ hosts/maple/configuration.nix
@@ -78,8 +78,6 @@
     };
     # kernelPackages = pkgs.linuxPackages_zen;
     kernelPackages = pkgs.linuxPackages_xanmod;
-    # suspend to RAM (s3 / deep)
-    kernelParams = ["mem_sleep_default=deep"];
   };
   catppuccin.grub.enable = true;
 
@@ -199,6 +197,9 @@
       hardware.openrazer.enable = lib.mkForce false;
       virtualisation.libvirtd.enable = lib.mkForce false;
       programs.gnupg.agent.enable = lib.mkForce false;
+
+      # suspend to RAM (s3 / deep)
+      boot.kernelParams = ["mem_sleep_default=deep"];
     };
     # vfio.configuration = {
     #   system.nixos.tags = ["with-vfio"];
