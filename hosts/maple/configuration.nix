49997fc475332db56cf5d3d2b8e13c2cefbd711b
Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>
2026-04-25 21:54:47 +0530

    add cuda binary cache
    Signed-off-by: Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>

--- hosts/maple/configuration.nix
+++ hosts/maple/configuration.nix
@@ -47,9 +47,11 @@
       flake-registry = "";
       nix-path = lib.mapAttrsToList (n: _: "${n}=flake:${n}") inputs;
       extra-substituters = [
+        "https://cache.nixos-cuda.org"
         "https://nix-community.cachix.org"
       ];
       extra-trusted-public-keys = [
+        "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
       trusted-users = [userSettings.username];
