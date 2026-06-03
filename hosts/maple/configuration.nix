00cc88b9b2c44eda82a89e6fa0f2db4ef1bcf6bd
Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>
2026-06-03 14:01:45 +0530

    remove unused caches
    Signed-off-by: Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>

--- hosts/maple/configuration.nix
+++ hosts/maple/configuration.nix
@@ -52,12 +52,10 @@
       extra-substituters = [
         "https://cache.nixos-cuda.org"
         "https://nix-community.cachix.org"
-        "https://arboretum.cachix.org"
       ];
       extra-trusted-public-keys = [
         "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "arboretum.cachix.org-1:FelD4HrR/RlPiaVu16hJaJ2uUPr1CXba8W2tmN7SpZo="
       ];
       trusted-users = [userSettings.username];
     };
