fbe438d0230042b67862c1ef9de6cd48056a358e
Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>
2026-06-09 00:11:28 +0530

    use 'let ... in' for androidsdk
    Signed-off-by: Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>

--- modules/system/app/android.nix
+++ modules/system/app/android.nix
@@ -2,25 +2,27 @@
   pkgs,
   userSettings,
   ...
-}: {
+}: let
+  androidComposition = pkgs.androidenv.composeAndroidPackages {
+    repoJson = ./repo.json;
+    # numLatestPlatformVersions = 2;
+    platformVersions = ["36.1" "37"];
+    includeEmulator = "if-supported";
+    includeSystemImages = "if-supported";
+    includeSources = true;
+    useGoogleAPIs = true;
+    systemImageTypes = ["google_apis"];
+    abiVersions = ["x86_64" "arm64-v8a"];
+    includeNDK = "if-supported";
+  };
+in {
   config = {
     nixpkgs.config.android_sdk.accept_license = true;
 
     environment.systemPackages = [
       pkgs.android-tools
 
-      (pkgs.androidStudioPackages.beta.withSdk
-        (pkgs.androidenv.composeAndroidPackages {
-          repoJson = ./repo.json;
-          numLatestPlatformVersions = 2;
-          includeEmulator = "if-supported";
-          includeSystemImages = "if-supported";
-          includeSources = true;
-          useGoogleAPIs = true;
-          systemImageTypes = ["google_apis"];
-          abiVersions = ["x86_64" "arm64-v8a"];
-          includeNDK = "if-supported";
-        }).androidsdk)
+      (pkgs.androidStudioPackages.beta.withSdk androidComposition.androidsdk)
     ];
     users.users.${userSettings.username} = {extraGroups = ["adbusers" "kvm"];};
   };
