76845c3107b2858ecdb35275598c8d50086da92c
Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>
2026-06-16 00:09:37 +0530

    Android-Studio: Remove nix configured androidsdk. Maybe not everything should be done via nix. Material icons made me give up. Why does it need to be in the sdk directory? @Jetbrains
    Signed-off-by: Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>

--- modules/system/app/android.nix
+++ modules/system/app/android.nix
@@ -2,28 +2,31 @@
   pkgs,
   userSettings,
   ...
-}: let
-  androidComposition = pkgs.androidenv.composeAndroidPackages {
-    repoJson = ./repo.json;
-    # numLatestPlatformVersions = 2;
-    platformVersions = ["36.1" "37"];
-    buildToolsVersions = ["36.0.0" "36.1.0" "37.0.0"];
-    includeEmulator = "if-supported";
-    includeSystemImages = "if-supported";
-    includeSources = true;
-    useGoogleAPIs = true;
-    systemImageTypes = ["google_apis"];
-    abiVersions = ["x86_64" "arm64-v8a"];
-    includeNDK = "if-supported";
-  };
-in {
+}:
+# let
+#   androidComposition = pkgs.androidenv.composeAndroidPackages {
+#     repoJson = ./repo.json;
+#     # numLatestPlatformVersions = 2;
+#     platformVersions = ["36.1" "37"];
+#     buildToolsVersions = ["36.0.0" "36.1.0" "37.0.0"];
+#     includeEmulator = "if-supported";
+#     includeSystemImages = "if-supported";
+#     includeSources = true;
+#     useGoogleAPIs = true;
+#     systemImageTypes = ["google_apis"];
+#     abiVersions = ["x86_64" "arm64-v8a"];
+#     includeNDK = "if-supported";
+#   };
+# in
+{
   config = {
-    nixpkgs.config.android_sdk.accept_license = true;
+    # nixpkgs.config.android_sdk.accept_license = true;
 
     environment.systemPackages = [
       pkgs.android-tools
 
-      (pkgs.androidStudioPackages.stable.withSdk androidComposition.androidsdk)
+      # (pkgs.androidStudioPackages.stable.withSdk androidComposition.androidsdk)
+      pkgs.androidStudioPackages.stable
     ];
     users.users.${userSettings.username} = {extraGroups = ["adbusers" "kvm"];};
   };
